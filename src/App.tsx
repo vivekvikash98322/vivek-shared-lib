bca00f9a595e3a08d539dfcc79fdf5af4d840aba
vivek kumar <vivekkumarsingh700@gmail.com>
2022-01-20 21:39:46 +0530

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import { Button, Form, Row, Col } from "react-bootstrap";
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,9 @@
 import './index.css';
-import App from './App';
+import App  from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
+export  { default as DataTableBuilder}  from "./components/data-table/data-table";
+export  { default as ACCORDATION}  from "./components/data-table/data-table";
+
 export {
   App
 }
