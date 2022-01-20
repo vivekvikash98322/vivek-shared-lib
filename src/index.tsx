4e2368c86027b654b25e5abbf1189b0927d041c9
vivek kumar <vivekkumarsingh700@gmail.com>
2022-01-20 21:50:09 +0530

    tes3

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import './index.css';
 import App  from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 export  { default as DataTableBuilder}  from "./components/data-table/data-table";
-export  { default as ACCORDATION}  from "./components/data-table/data-table";
+export  { default as ACCORDATION}  from "./components/accordation/accordation";
 
 export {
   App
